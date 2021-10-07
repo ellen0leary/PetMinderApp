bde38a053dd0fe5220c80612f6ca34d47cb89ac6
Ellen <eol50559@gmail.com>
2021-10-07 11:52:40 +0100

    add basic menu function and validation

--- src/main/kotlin/org/wit/petMinder/console/main/Main.kt
+++ src/main/kotlin/org/wit/petMinder/console/main/Main.kt
@@ -6,4 +6,26 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
  logger.info{"Launching PetMinder Console"}
+ println("PetMinder Kotlin App Version 1.0")
+
+ var input : Int
+ input = menu()
+}
+
+fun menu() : Int {
+ var option : Int
+ var input: String ? = null
+
+ println("MAIN MENU")
+ println(" 1. Add Pet")
+ println("-1. Exit")
+ println()
+ print("Enter an integer : ")
+ input = readLine()!!
+ option = if(input.toIntOrNull() != null && !input.isEmpty())
+      input.toInt()
+ else
+      -9
+
+ return option
 }
