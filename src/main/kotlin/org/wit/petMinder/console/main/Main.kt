b91add8bc27e71a5f09e108c608a2db24fba4102
Ellen <eol50559@gmail.com>
2021-10-13 14:43:52 +0100

    added searching functionality

--- src/main/kotlin/org/wit/petMinder/console/main/Main.kt
+++ src/main/kotlin/org/wit/petMinder/console/main/Main.kt
@@ -4,73 +4,120 @@ import mu.KotlinLogging
 import org.wit.petMinder.console.models.PetModel
 import java.awt.SystemColor.menu
 
-var pet = PetModel()
 val pets = ArrayList<PetModel>()
 private val logger = KotlinLogging.logger {}
 fun main(args: Array<String>) {
- logger.info{"Launching PetMinder Console"}
- println("PetMinder Kotlin App Version 1.0")
+    logger.info { "Launching PetMinder Console" }
+    println("PetMinder Kotlin App Version 1.0")
 
- var input : Int
- do {
-  input = menu()
-  when(input){
-   1 -> addPet()
-   2-> updatePet()
-   3-> listPets()
-   -1 -> println("Exiting App")
-   else -> println("Invalid Option")
-  }
- } while (input!= -1)
- logger.info{"Shutting Down Petminder Console App"}
+    var input: Int
+    do {
+        input = menu()
+        when (input) {
+            1 -> addPet()
+            2 -> updatePet()
+            3 -> listPets()
+            4 -> searchPet()
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+    } while (input != -1)
+    logger.info { "Shutting Down Petminder Console App" }
 }
 
-fun menu() : Int {
- var option : Int
- var input: String ? = null
+fun menu(): Int {
+    var option: Int
+    var input: String? = null
 
- println("MAIN MENU")
- println(" 1. Add Pet")
- println(" 2. Update Pet")
- println(" 3. List All Pet")
- println("-1. Exit")
- println()
- print("Enter an integer : ")
- input = readLine()!!
- option = if(input.toIntOrNull() != null && !input.isEmpty())
-      input.toInt()
- else
-      -9
+    println("MAIN MENU")
+    println(" 1. Add Pet")
+    println(" 2. Update Pet")
+    println(" 3. List All Pet")
+    println("-1. Exit")
+    println()
+    print("Enter an integer : ")
+    input = readLine()!!
+    option = if (input.toIntOrNull() != null && !input.isEmpty())
+        input.toInt()
+    else
+        -9
 
- return option
+    return option
 }
 
-fun addPet(){
- println("Add Placemark")
- println()
- println("Enter a Name: ")
- pet.name = readLine()!!
+fun addPet() {
+    var aPet = PetModel()
+    println("Add Placemark")
+    println()
+    println("Enter a Name: ")
+    aPet.name = readLine()!!
 
- println("Enter date of birth")
- pet.dob = readLine()!!
- println("You enterec ["+ pet.name +"]for name and ["+pet.dob+"] for date of birth")
- pets.add(pet.copy())
+    println("Enter date of birth")
+    aPet.dob = readLine()!!
+   if(aPet.name.isNotEmpty() && aPet.dob.isNotEmpty()) {
+       aPet.id = pets.size.toLong()
+       pets.add(aPet.copy())
+       logger.info{"Placemark Added : [ $aPet ]"}
+   } else {
+       logger.info("Placemark Not Added")
+   }
+}
+
+fun updatePet() {
+    println("Update Placemark")
+    println()
+    listPets()
+    var searckId = getId()
+    val aPet = search(searckId)
+
+    if(aPet!= null){
+        print("emter a new name for ["+ aPet.name+"")
+        aPet.name = readLine()!!
+        println("Enter a new Description for [ "+aPet.dob+"")
+        aPet.dob = readLine()!!
+        println(
+            "You updated [ + aPlacemark.title +  ] for title  " +
+                "and [  + aPlacemark.description +  ] for description" )
+    } else {
+        println("Placemark Not Updated")
+    }
+}
+
+fun listPets() {
+    println("List All Placemarks")
+    println()
+    pets.forEach { logger.info("${it}") }
+    println()
+}
+
+fun searchPet() {
+    var searchId = getId()
+    val aPet = search(searchId)
+    if (aPet != null)
+        println("Pet Details [ $aPet]")
+    else
+        println("Pet not found")
 
 }
 
-fun updatePet(){
- println("Update Placemark")
- println()
- println("Enter a new Name for "+ pet.name+": ")
- pet.name = readLine()!!
+fun getId(): Long {
+    var strID: String?
+    var searchId: Long
+    print("Enter id to Search/Update: ")
+    strID = readLine()!!
+    searchId = if (strID.toLongOrNull() != null && !strID.isEmpty())
+        strID.toLong()
+    else
+        -9
 
- println("Enter a new date of birth for "+pet.name+": " )
- pet.dob = readLine()!!
- println("You enterec ["+ pet.name +"]for name and ["+pet.dob+"] for date of birth")
+    return searchId
 }
 
-fun listPets(){
- println("List All Placemarks")
- println()
- pets.forEach{logger.info("${it}")}
+fun search(id: Long): PetModel? {
+    var foundPet: PetModel? = pets.find { p -> p.id == id }
+    return foundPet
+}
+
+fun dummyDate(){
+    //add later
 }
--- src/main/kotlin/org/wit/petMinder/console/models/PetModel.kt
+++ src/main/kotlin/org/wit/petMinder/console/models/PetModel.kt
@@ -1,4 +1,5 @@
 package org.wit.petMinder.console.models
 
- data class PetModel(var name: String="",
+ data class PetModel(var id: Long = 0,
+                    var name: String="",
                      var dob: String="")
