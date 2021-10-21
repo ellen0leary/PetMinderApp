3340c6938179c897c3e8d6ef739282634a8a77fc
Ellen <eol50559@gmail.com>
2021-10-21 13:13:26 +0100

    Added Color to console output

--- src/main/kotlin/org/wit/petMinder/console/views/MainView.kt
+++ src/main/kotlin/org/wit/petMinder/console/views/MainView.kt
@@ -2,32 +2,40 @@ package org.wit.petMinder.console.views
 
 class MainView {
 
+    val TEXT_RESET = "\u001B[0m"
+    val PURPLE = "\u001b[1;35m"
+    val CYAN = "\u001b[0;36m"
+    val BLUE = "\u001b[0;34m"
+    val GREEN = "\u001b[0;32m"
+    val YELLOW = "\u001b[0;33m"
+
+//    val CYAN ="\033[1;36m"
     fun menu() : Int {
         var option : Int
         var input: String?
 
-        println("MAIN MENU")
-        println(" 1. Add Pet")
+        println(PURPLE+ "MAIN MENU"+ TEXT_RESET)
+        println(CYAN +" 1. Add Pet" )
         println(" 2. Update Pet")
         println(" 3. List All Pets")
         println(" 4. Search Pet")
-        println(" 5. Delete Pet")
-        println("-------------------")
-        println(" 6. Add Feed")
+        println(" 5. Delete Pet"+ TEXT_RESET)
+        println(PURPLE+ "-------------------" + TEXT_RESET)
+        println(BLUE +" 6. Add Feed")
         println(" 7. Update Feed")
         println(" 8. List All Feed")
         println(" 9. Search Feed")
-        println("10. Delete Feed")
-        println("-------------------")
-        println("11. Add Exercise")
+        println("10. Delete Feed" + TEXT_RESET)
+        println(PURPLE+ "-------------------" + TEXT_RESET)
+        println(GREEN + "11. Add Exercise")
         println("12. Update Exercise")
         println("13. List All Exercise")
         println("14. Search Exercise")
-        println("15. Delete Exercise")
-        println("-------------------")
-        println("16. See All Pets Data")
+        println("15. Delete Exercise"+ TEXT_RESET)
+        println(PURPLE+ "-------------------" + TEXT_RESET)
+        println(YELLOW + "16. See All Pets Data")
         println("17. See Pets Exercise")
-        println("18. See Pets Feeds")
+        println("18. See Pets Feeds"+ TEXT_RESET)
         println("99. Add Dummy Data")
         println("-1. Exit")
         println()
--- src/test/kotlin/org/wit/petMinder/console/models/PetJSONStoreTest.kt
+++ src/test/kotlin/org/wit/petMinder/console/models/PetJSONStoreTest.kt
@@ -3,7 +3,6 @@ package org.wit.petMinder.console.models
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import java.util.*
 import kotlin.random.Random
 
 class PetJSONStoreTest {
